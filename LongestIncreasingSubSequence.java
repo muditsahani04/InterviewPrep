61fdaeb0e5fd00460b2a82aaa4a58e65f24086b3
muditsahani04 <38572844+muditsahani04@users.noreply.github.com>
2018-05-17 19:01:51 -0700

    Longest Increasing Subsequence LeetCode

---
+++ LongestIncreasingSubSequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        
+		int[] dp = new int[nums.length];
+		int len = 0;
+		for (int i=0; i<nums.length; i++)
+		{
+			int index = Arrays.binarySearch(dp, 0, len, nums[i]);
+			if (index<0)
+				index = -(index + 1);
+			
+			dp[index] = nums[i];
+			if (index == len)
+				len++;
+		} 
+		
+		return len;
+	}
+}
