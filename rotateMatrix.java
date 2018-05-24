c2e75301dec399194fd4b532176a46506cf9701e
muditsahani04 <38572844+muditsahani04@users.noreply.github.com>
2018-05-24 00:26:39 -0700

    Rotate Matrix (Better)

--- rotateMatrix.java
+++ rotateMatrix.java
@@ -1,42 +1,24 @@
 
-public class rotateMatrix {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	int[][] m = {{1,2,3},{4,5,6},{7,8,9}};
-	System.out.println("Original Matrix:");
-	printMatrix(m);
-	rotate(m);
-	System.out.println("Rotated Matrix:");
-	printMatrix(m);
-	}
-
-	public static void printMatrix(int[][] Matrix)
-	{
-		for (int i=0 ; i<Matrix.length ;i++)
-		{
-			for (int j=0; j<Matrix[0].length; j++)
-			{
-				System.out.print(Matrix[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
-	public static void rotate(int Matrix[][])
-	{
-		for (int layer=0; layer < Matrix.length; layer++)
-		{
-			int first = layer;
-			int last = Matrix.length - layer - 1;
-			for (int j=layer; j < last; j++)
-			 {
-				int offset = j-layer;
-				int temp = Matrix[first][j];
-				Matrix[first][j] = Matrix[last-offset][first];
-				Matrix[last-offset][first] = Matrix[last][last-offset];
-				Matrix[last][last-offset] = Matrix[j][last];
-				Matrix[j][last] = temp;
-			 }
-		}
-	}
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int r1=0, r2 = matrix.length-1, c1=0, c2 = matrix[0].length-1;
+        
+        while (r1<r2 && c1<c2)
+        {
+            for (int r=r1,c=c2; r<r2 && c>c1; r++,c--)
+            {
+                int temp = matrix[r1][r];
+                matrix[r1][r] = matrix[c][r1];
+                matrix[c][r1] = matrix[r2][c];
+                matrix[r2][c] = matrix[r][c2];
+                matrix[r][c2] = temp;
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+            
+        } 
+    }
 }
