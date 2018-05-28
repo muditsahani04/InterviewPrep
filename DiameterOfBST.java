e3954510fe94e51893f76c1c3cbadc1482a53a29
muditsahani04 <38572844+muditsahani04@users.noreply.github.com>
2018-05-28 12:40:18 -0700

    Diameter of BST

--- DiameterOfBST.java
+++ DiameterOfBST.java
@@ -8,27 +8,25 @@
  * }
  */
 class Solution {
+   
     int max = Integer.MIN_VALUE;
     public int diameterOfBinaryTree(TreeNode root) {
+    
         if (root == null)
             return 0;
-        int sum = helper(root.left) + helper(root.right);
-        if ( sum > max)
-            max = sum;
-        diameterOfBinaryTree(root.left);
-        diameterOfBinaryTree(root.right);
-        
+        maxDepth(root);
         return max;
+        
     }
     
-    private int helper(TreeNode root)
+    private int maxDepth(TreeNode root)
     {
         if (root == null)
             return 0;
         
-        int left = helper(root.left);
-        int right = helper(root.right);
-        
-        return Math.max(left,right)+1;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        max = Math.max(left+right, max);
+        return Math.max(left, right) +1;
     }
 }
