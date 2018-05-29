4ee4a3bce223c9fe8f7436ea1d59da600f698c52
muditsahani04 <38572844+muditsahani04@users.noreply.github.com>
2018-05-28 22:22:50 -0700

    InOrder Successor of BST (updated)

--- InOrderSuccessor.java
+++ InOrderSuccessor.java
@@ -9,47 +9,41 @@
  */
 class Solution {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-       
-        return inOrderHelper(root,p,root);
-    }
-    
-    private TreeNode inOrderHelper(TreeNode root, TreeNode p, TreeNode q)
-    {
-        if (root == null || p == null)
-            return null;
-        if (root == p)
-        {
-            if (root.right == null)
-            {
-                if ( q.val > root.val )
-                    return q;
-                else
-                    return null;
-            }
-            else 
-            {
-                return minInRightSubTree(root.right);
-                
-            }
-        }
         
-        if (p.val < root.val)
+        if (root == null || p ==null)
+            return null;
+        if (p.right == null)
         {
-            q =  root;
-            return inOrderHelper(root.left, p, q);
+            TreeNode succ = null;
+            TreeNode current = root;
+            while (current!= null && current.val!= p.val)
+            {
+                if (p.val < current.val)
+                {
+                    succ = current;
+                    current = current.left;
+                }
+            else if (p.val > current.val)
+                {
+                    current = current.right;
+                } 
+            }
+            
+            return succ;
+            
         }
         else
         {
-            
-            return inOrderHelper(root.right, p, q);
+            return minInRightSubTree(p.right);
         }
     }
     
     private TreeNode minInRightSubTree(TreeNode root)
     {
-        if ( root.left != null)
-            return minInRightSubTree(root.left);
-        return root;
+        if (root.left == null)
+            return root;
         
+        return  minInRightSubTree(root.left);
     }
+    
 }
